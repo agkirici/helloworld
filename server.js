264c6186fe57e2b894cd037f8dcbf183efad1425
Arzu Kirirci <agkirici@myseneca.ca>
2022-09-15 15:47:02 -0400

    uptade server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Arzu Gizem Kirici - 135304210");
 });
 
 // setup http server to listen on HTTP_PORT
